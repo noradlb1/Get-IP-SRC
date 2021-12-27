ea024e94a592e0115ce169fb71597d7d35093734
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-27 06:12:40 +0100

    Get-IP-SRC
    Get-IP-SRC

---
+++ Master/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ButterscotchTheme1 = New Master.ButterscotchTheme()
+        Me.ButterscotchControlBox1 = New Master.ButterscotchControlBox()
+        Me.ButterscotchButton1 = New Master.ButterscotchButton()
+        Me.TextBox2 = New Master.ButterscotchTextBox()
+        Me.TextBox1 = New Master.ButterscotchTextBox()
+        Me.ButterscotchHorizontalSeperator1 = New Master.ButterscotchHorizontalSeperator()
+        Me.ButterscotchTheme1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ButterscotchTheme1
+        '
+        Me.ButterscotchTheme1.Border = True
+        Me.ButterscotchTheme1.Controls.Add(Me.ButterscotchControlBox1)
+        Me.ButterscotchTheme1.Controls.Add(Me.ButterscotchButton1)
+        Me.ButterscotchTheme1.Controls.Add(Me.TextBox2)
+        Me.ButterscotchTheme1.Controls.Add(Me.TextBox1)
+        Me.ButterscotchTheme1.Controls.Add(Me.ButterscotchHorizontalSeperator1)
+        Me.ButterscotchTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButterscotchTheme1.Icon = Nothing
+        Me.ButterscotchTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.ButterscotchTheme1.Name = "ButterscotchTheme1"
+        Me.ButterscotchTheme1.Size = New System.Drawing.Size(260, 156)
+        Me.ButterscotchTheme1.TabIndex = 0
+        Me.ButterscotchTheme1.Text = "MONSTERMC"
+        '
+        'ButterscotchControlBox1
+        '
+        Me.ButterscotchControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButterscotchControlBox1.BackColor = System.Drawing.Color.Transparent
+        Me.ButterscotchControlBox1.Location = New System.Drawing.Point(173, 0)
+        Me.ButterscotchControlBox1.MaximumDisable = False
+        Me.ButterscotchControlBox1.MinimumDisable = False
+        Me.ButterscotchControlBox1.Name = "ButterscotchControlBox1"
+        Me.ButterscotchControlBox1.Size = New System.Drawing.Size(87, 30)
+        Me.ButterscotchControlBox1.TabIndex = 6
+        Me.ButterscotchControlBox1.Text = "X"
+        '
+        'ButterscotchButton1
+        '
+        Me.ButterscotchButton1.BackColor = System.Drawing.Color.Transparent
+        Me.ButterscotchButton1.Location = New System.Drawing.Point(33, 79)
+        Me.ButterscotchButton1.Name = "ButterscotchButton1"
+        Me.ButterscotchButton1.Size = New System.Drawing.Size(199, 35)
+        Me.ButterscotchButton1.TabIndex = 4
+        Me.ButterscotchButton1.Text = "GRAP"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(48, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 11.0!)
+        Me.TextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextBox2.Location = New System.Drawing.Point(33, 120)
+        Me.TextBox2.MaxLength = 32767
+        Me.TextBox2.MultiLine = False
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = False
+        Me.TextBox2.Size = New System.Drawing.Size(199, 23)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "IP"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox2.UseSystemPasswordChar = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(48, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(39, Byte), Integer))
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 11.0!)
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(33, 50)
+        Me.TextBox1.MaxLength = 32767
+        Me.TextBox1.MultiLine = False
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = False
+        Me.TextBox1.Size = New System.Drawing.Size(199, 23)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "Host + هوست"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox1.UseSystemPasswordChar = False
+        '
+        'ButterscotchHorizontalSeperator1
+        '
+        Me.ButterscotchHorizontalSeperator1.BackColor = System.Drawing.Color.Transparent
+        Me.ButterscotchHorizontalSeperator1.Location = New System.Drawing.Point(33, 41)
+        Me.ButterscotchHorizontalSeperator1.Name = "ButterscotchHorizontalSeperator1"
+        Me.ButterscotchHorizontalSeperator1.Size = New System.Drawing.Size(199, 3)
+        Me.ButterscotchHorizontalSeperator1.TabIndex = 1
+        Me.ButterscotchHorizontalSeperator1.Text = "ButterscotchHorizontalSeperator1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(260, 156)
+        Me.Controls.Add(Me.ButterscotchTheme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ButterscotchTheme1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ButterscotchTheme1 As Master.ButterscotchTheme
+    Friend WithEvents ButterscotchButton1 As Master.ButterscotchButton
+    Friend WithEvents TextBox2 As Master.ButterscotchTextBox
+    Friend WithEvents TextBox1 As Master.ButterscotchTextBox
+    Friend WithEvents ButterscotchHorizontalSeperator1 As Master.ButterscotchHorizontalSeperator
+    Friend WithEvents ButterscotchControlBox1 As Master.ButterscotchControlBox
+
+End Class
---
+++ Master/Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.CompilerServices
+Imports System
+Imports System.ComponentModel
+Imports System.Diagnostics
+Imports System.Drawing
+Imports System.Net
+Imports System.Runtime.CompilerServices
+Imports System.Windows.Forms
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ButterscotchTheme1_Click(sender As Object, e As EventArgs) Handles ButterscotchTheme1.Click
+
+    End Sub
+
+    Private Sub ButterscotchButton1_Click(sender As Object, e As EventArgs) Handles ButterscotchButton1.Click
+        Try
+            If (Me.TextBox1.Text = "") Then
+                Interaction.MsgBox("Please Add Host !!", MsgBoxStyle.Information, Nothing)
+            End If
+            Dim addressList As IPAddress() = Dns.GetHostByName(Me.TextBox1.Text).AddressList
+            Me.TextBox2.Text = addressList(0).ToString
+        Catch exception1 As Exception
+            ProjectData.SetProjectError(exception1)
+            Dim exception As Exception = exception1
+            ProjectData.ClearProjectError()
+        End Try
+    End Sub
+End Class
